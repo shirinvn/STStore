01914b39a31f46b42b199a1bd2e5527123c1b017
shirinvatani <shirinvatani97@gmail.com>
2023-11-09 02:07:53 +0330

    fix bug

--- app/src/main/java/com/example/stsotre/ui/screens/home/AmazingItem.kt
+++ app/src/main/java/com/example/stsotre/ui/screens/home/AmazingItem.kt
@@ -47,132 +47,166 @@ import com.example.stsotre.util.DigitHelper.digitByLocate
 import com.example.stsotre.util.DigitHelper.digitByLocateAndSeparator
 
 @Composable
-fun AmazingItem(item :AmazingItem){
+fun AmazingItem(item: AmazingItem) {
 
-    Card( modifier = Modifier.padding(vertical = MaterialTheme.spacing.semilarge,
-        horizontal = MaterialTheme.spacing.extraSmall),
-        shape = MaterialTheme.roundedShape.small) {
+    Card(
+        modifier = Modifier
+            .width(170.dp)
+            .padding(
+                vertical = MaterialTheme.spacing.semilarge,
+                horizontal = MaterialTheme.spacing.semismall
+            ),
+        shape = MaterialTheme.roundedShape.small,
+    ) {
 
-        Column (modifier = Modifier
-            .fillMaxSize()
-            .padding(vertical = MaterialTheme.spacing.small)){
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = MaterialTheme.spacing.small)
+        ) {
 
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = MaterialTheme.spacing.extraSmall)
+            ) {
 
-            Column (modifier = Modifier
-                .fillMaxSize()
-                .padding(vertical = MaterialTheme.spacing.small)){
-
-                Text(text = stringResource(id = R.string.amazing_for_app),
+                Text(
+                    text = stringResource(id = R.string.amazing_for_app),
                     modifier = Modifier
                         .padding(start = MaterialTheme.spacing.small),
                     style = MaterialTheme.typography.extraSmall,
                     fontWeight = FontWeight.SemiBold,
-                    color= MaterialTheme.colors.DigiKalaRed)
+                    color = MaterialTheme.colors.DigiKalaRed,
+                )
 
                 Spacer(modifier = Modifier.height(10.dp))
 
-
-                Image(painter = rememberAsyncImagePainter(item.image), contentDescription = "",
+                Image(
+                    painter = rememberAsyncImagePainter(item.image),
+                    contentDescription = "",
                     modifier = Modifier
                         .fillMaxWidth()
                         .height(130.dp),
-                    contentScale = ContentScale.FillBounds)
+                    contentScale = ContentScale.FillBounds
+                )
 
             }
 
-
             Spacer(modifier = Modifier.height(10.dp))
-            Column(modifier = Modifier
-                .fillMaxWidth()
-                .padding(vertical = MaterialTheme.spacing.small)){
 
-                Text(text = item.name,
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = MaterialTheme.spacing.small)
+            ) {
+
+                Text(
+                    text = item.name,
                     modifier = Modifier
                         .fillMaxSize()
                         .height(48.dp)
                         .padding(horizontal = MaterialTheme.spacing.small),
-                    style = MaterialTheme.typography.h6
-                ,
+                    style = MaterialTheme.typography.h6,
                     fontWeight = FontWeight.SemiBold,
-                    color=MaterialTheme.colors.darkText,
+                    color = MaterialTheme.colors.darkText,
                     maxLines = 2,
                     overflow = TextOverflow.Ellipsis
                 )
+
                 Spacer(modifier = Modifier.height(10.dp))
 
-                Row (modifier = Modifier.fillMaxWidth(),
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth(),
                     horizontalArrangement = Arrangement.Center,
-                    verticalAlignment = Alignment.CenterVertically){
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
 
-
-                    Icon(painter = painterResource(id = R.drawable.in_stock), contentDescription ="",
-                        tint = MaterialTheme.colors.DarkCyan
-                        ,
+                    Icon(
+                        painter = painterResource(id = R.drawable.in_stock),
+                        contentDescription = "",
                         modifier = Modifier
                             .size(22.dp)
-                            .padding(2.dp))
+                            .padding(2.dp),
+                        tint = MaterialTheme.colors.DarkCyan
+                    )
                     Text(
                         text = item.seller,
                         style = MaterialTheme.typography.extraSmall,
                         fontWeight = FontWeight.SemiBold,
-                    color = MaterialTheme.colors.semiDarkText)
-                }
+                        color = MaterialTheme.colors.semiDarkText,
+                    )
 
+                }
 
                 Spacer(modifier = Modifier.height(10.dp))
 
-                Row (modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(horizontal = MaterialTheme.spacing.small),
+
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = MaterialTheme.spacing.small),
                     horizontalArrangement = Arrangement.SpaceBetween,
-                    verticalAlignment = Alignment.Top){
+                    verticalAlignment = Alignment.Top
+                ) {
 
-                    Box (modifier = Modifier
-                        .width(40.dp)
-                        .height(24.dp)
-                        .background(
-                            color = MaterialTheme.colors.DigikalaDarkRed,
-                            shape = CircleShape
-                        )
-                        .wrapContentWidth(Alignment.CenterHorizontally)
-                        .wrapContentHeight(Alignment.CenterVertically)){
-
-                        Text(text = "${digitByLocate(item.discountPercent.toString())}",
+                    Box(
+                        modifier = Modifier
+                            .width(40.dp)
+                            .height(24.dp)
+                            .background(
+                                color = MaterialTheme.colors.DigikalaDarkRed,
+                                shape = CircleShape
+                            )
+                            .wrapContentWidth(Alignment.CenterHorizontally)
+                            .wrapContentHeight(Alignment.CenterVertically)
+                    ) {
+                        Text(
+                            text = "${digitByLocate(item.discountPercent.toString())}%",
                             color = Color.White,
                             style = MaterialTheme.typography.h6,
-                            fontWeight = FontWeight.SemiBold)
+                            fontWeight = FontWeight.Bold,
+                        )
                     }
 
+
                     Column {
+
                         Row {
-                            Text(text = digitByLocateAndSeparator(applyDiscount(item.price, item.discountPercent).toString())
-                                , style = MaterialTheme.typography.body2,
+                            Text(
+                                text = digitByLocateAndSeparator(applyDiscount(item.price , item.discountPercent).toString()),
+                                style = MaterialTheme.typography.body2,
                                 fontWeight = FontWeight.SemiBold,
                             )
 
-                            Image(painter = painterResource(id = R.drawable.toman), contentDescription = ""
-                            , modifier = Modifier.size(MaterialTheme.spacing.semilarge)
+                            Image(
+                                painter = painterResource(id = R.drawable.toman),
+                                contentDescription = "",
+                                modifier = Modifier
+                                    .size(MaterialTheme.spacing.semilarge)
                                     .padding(horizontal = MaterialTheme.spacing.extraSmall)
                             )
+
                         }
 
-                        Text(text = digitByLocateAndSeparator(item.price.toString()),
+                        Text(
+                            text = digitByLocateAndSeparator(item.price.toString()),
                             color = Color.LightGray,
                             style = MaterialTheme.typography.body2,
                             textDecoration = TextDecoration.LineThrough
-
-                            )
+                        )
                     }
-                }
-
-
 
                 }
 
 
             }
 
-    }
 
+        }
+
+
+    }
 
 }
