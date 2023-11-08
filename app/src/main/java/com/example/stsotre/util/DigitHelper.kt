54aec4db9a378b44e654753c95d56245d9062a35
shirinvatani <shirinvatani97@gmail.com>
2023-11-08 23:58:50 +0330

    persion digit

---
+++ app/src/main/java/com/example/stsotre/util/DigitHelper.kt
@@ -0,0 +1,45 @@
+package com.example.stsotre.util
+
+import java.text.DecimalFormat
+
+object DigitHelper {
+
+
+
+    fun digitByLocate(englishStr: String): String{
+        var result = ""
+        var fa = '۰'
+        for (ch in englishStr){
+            when (ch){
+                '۰'-> fa = '0'
+                '۱'-> fa = '1'
+                '۲'-> fa = '2'
+                '۳'-> fa = '3'
+                '۴'-> fa = '4'
+                '۵'-> fa = '5'
+                '۶'-> fa = '6'
+                '۷'-> fa = '7'
+                '۸'-> fa = '8'
+                '۹'-> fa = '9'
+
+            }
+
+            result = "${result}$fa"
+        }
+        return result
+    }
+
+
+    //جداسازی سه تایی ارقام از هم
+
+    fun digitBySeparator(price : String) : String{
+        val priceFormat = DecimalFormat("###,###")
+        return priceFormat.format(Integer.valueOf(price))
+    }
+
+    fun digitByLocateAndSeparator(price: String): String{
+        val pricewithoutCommas = price.replace(",","")
+        val presianDigit= digitByLocate(pricewithoutCommas)
+        return digitBySeparator(presianDigit)
+    }
+}
