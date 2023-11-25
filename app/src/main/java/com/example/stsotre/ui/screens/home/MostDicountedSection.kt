47fa338fc2ac0fb92a9c8a7a8423b601a330abfa
shirinvatani <shirinvatani97@gmail.com>
2023-11-26 03:27:24 +0330

    Most Disconut list

--- app/src/main/java/com/example/stsotre/ui/screens/home/MostDicountedSection.kt
+++ app/src/main/java/com/example/stsotre/ui/screens/home/MostDicountedSection.kt
@@ -1,18 +1,34 @@
 package com.example.stsotre.ui.screens.home
 
 import android.util.Log
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
+import androidx.compose.foundation.layout.FlowRow
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.stsotre.R
 import com.example.stsotre.data.model.home.StoreProduct
 import com.example.stsotre.data.remote.NetWorkResult
+import com.example.stsotre.ui.theme.darkText
+import com.example.stsotre.ui.theme.spacing
 import com.example.stsotre.viewmodel.HomeViewModel
 
+@OptIn(ExperimentalLayoutApi::class)
 @Composable
 fun MostDicountedSection (
     viewModel: HomeViewModel = hiltViewModel()
@@ -44,6 +60,41 @@ fun MostDicountedSection (
             loading = true
         }
     }
+
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+    ) {
+
+
+        Text(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(MaterialTheme.spacing.small),
+            text = stringResource(id = R.string.most_discounted_products),
+            textAlign = TextAlign.Right,
+            style = MaterialTheme.typography.h3,
+            fontWeight = FontWeight.SemiBold,
+            color = MaterialTheme.colors.darkText,
+        )
+
+        FlowRow(
+            maxItemsInEachRow = 2,
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentHeight(),
+            horizontalArrangement = Arrangement.Center
+        ) {
+
+            for (item in mostDicountedlist) {
+                MostDiscountedCard(item)
+            }
+
+        }
+
+
+
+}
 
 
 
